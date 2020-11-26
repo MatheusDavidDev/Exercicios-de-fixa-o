78574faf9b2d82c8f3339609a8ab6c36e49457d5
matheus <matheusviniciusdavid@gmail.com>
2020-11-26 20:40:21 -0300

    Completo

--- Exercicio 4/Program.cs
+++ Exercicio 4/Program.cs
@@ -7,6 +7,9 @@ namespace Exercicio_4
         static void Main(string[] args)
         {   
             int[] valores = new int[10];
+            //int a;
+            int maior = 0;
+            int menor = 0;
 
             Console.WriteLine("Digite 10 valores");
 
@@ -16,7 +19,27 @@ namespace Exercicio_4
                 valores[contador] = int.Parse(Console.ReadLine());
             }
             
-            
+            Console.WriteLine("Voce digitou esses numeros");
+
+            for (var contador = 0; contador < 10; contador++)
+            {
+                Console.Write(+valores[contador]+", ");
+                if (contador==0){maior=valores[contador]; menor=valores[contador];}
+                if (valores[contador]>maior)
+                {
+                    maior = valores[contador];
+                }
+                else{
+                    if (valores[contador] <menor)
+                    {
+                        menor=valores[contador];
+                    }
+                }
+
+            }
+                Console.WriteLine("O maior valor digitado e de: "+maior );
+                Console.WriteLine("O menor valor digitado e de: "+menor );
+
         }
     }
 }
--- Exercicio 7/Program.cs
+++ Exercicio 7/Program.cs
@@ -13,10 +13,21 @@ namespace Exercicio_7
             {
                 Console.WriteLine("Digite o numero "+contador);
                 numeros[contador] = int.Parse(Console.ReadLine());
-
-                Console.WriteLine();
             } 
-            Console.WriteLine(numeros[4], numeros[3], numeros[2], numeros[1], numeros[0]);
+
+            int[] numerosInver = new int[numeros.Length];
+            
+            for (var contador = 0; contador < numeros.Length; contador++)
+            {
+                numerosInver[contador] = numeros[numeros.Length -1 -contador];
+            }
+
+            for (var contador = 0; contador < numerosInver.Length; contador++)
+            {
+                int invertido = numerosInver[contador];
+                Console.WriteLine($"a sequenci invertida e de: {invertido}");
+            }
+
             
         }
     }
